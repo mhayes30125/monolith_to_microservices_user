32995067767a580ce9529a6dc5177cdcb067a873
Mike <mhayes30125@yahoo.com>
2021-01-01 13:48:45 -0500

    Applying all Feed changes to user

--- udagram-api/src/controllers/v0/index.router.ts
+++ udagram-api/src/controllers/v0/index.router.ts
@@ -1,9 +1,11 @@
 import {Router, Request, Response} from 'express';
 import {UserRouter} from './users/routes/user.router';
+import {HealthcheckRouter} from './health/routes/healthcheck.router';
 
 const router: Router = Router();
 
 router.use('/users', UserRouter);
+router.use('/healthcheck', HealthcheckRouter);
 
 router.get('/', async (req: Request, res: Response) => {
   res.send(`V0`);
